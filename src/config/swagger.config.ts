5aaecc49dac282fcea427139bfad7d7c96a128bf
martingaidoo <martingaido00@gmail.com>
2025-10-16 20:33:27 -0300

    se agrega la configuracion de la documentacion de la api

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,20 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-export function setupSwagger(app: INestApplication) {
-	const config = new DocumentBuilder()
-		.setTitle('SafeStore API')
-		.setDescription('Documentación automática de la API SafeStore')
-		.setVersion('1.0')
-		.addBearerAuth({
-			type: 'http',
-			scheme: 'bearer',
-			bearerFormat: 'JWT',
-		}, 'access-token')
-		.build();
+export function setupSwagger(app: INestApplication): void {
+  const config = new DocumentBuilder()
+    .setTitle('API Swagger')
+    .setDescription('Documentación de endpoints de la API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup('api/docs', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 }
-
-export default setupSwagger;
